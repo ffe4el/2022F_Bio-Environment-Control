2d01c73a4f6df552b3db92d23d64c73b26d00482
Jung Jaeyoung <93760723+jungjae0@users.noreply.github.com>
2022-10-13 15:22:53 +0900

    Update value_print.py

--- chamber_jaeyoung/value_print.py
+++ chamber_jaeyoung/value_print.py
@@ -25,30 +25,33 @@ def send_signal_to_sfarm(msg):
     if (s.readable()):
         s.write("{}\n".format(msg).encode())
 
+def send_signal_to_ssfarm(msg):
+    pass
+
 
 # 여름 1단계
 def summer_first():
     # fan
     send_signal_to_sfarm("C_F-0")
     # heat
-    send_signal_to_sfarm("RO")
+    send_signal_to_ssfarm("RO")
     # light
     send_signal_to_sfarm("C_L-9")
     # fog
-    send_signal_to_sfarm("B0")
+    send_signal_to_ssfarm("B0")
     # window
-    send_signal_to_sfarm("S1")
+    send_signal_to_sfarm("C_S-1")
 
 # 여름 2단계
 def summer_second():
     # fan
     send_signal_to_sfarm("C_F-1")
     # heat
-    send_signal_to_sfarm("RO")
+    send_signal_to_ssfarm("RO")
     # light
     send_signal_to_sfarm("C_L-6")
     # fog
-    send_signal_to_sfarm("B0")
+    send_signal_to_ssfarm("B0")
     # window
     send_signal_to_sfarm("S1")
 
@@ -56,210 +59,210 @@ def summer_second():
 # 여름 3-1단계
 def summer_third1():
     # fan
-    send_signal_to_sfarm("C_F-1")
+    send_signal_to_ssfarm("C_F-1")
     # heat
     send_signal_to_sfarm("RO")
     # light
     send_signal_to_sfarm("C_L-7")
     # fog
-    send_signal_to_sfarm("B0")
+    send_signal_to_ssfarm("B0")
     # window
-    send_signal_to_sfarm("S1")
+    send_signal_to_sfarm("C_S-1")
 
 # 여름 3-2단계
 def summer_third2():
     # fan
     send_signal_to_sfarm("C_F-1")
     # heat
-    send_signal_to_sfarm("RO")
+    send_signal_to_ssfarm("RO")
     # light
     send_signal_to_sfarm("C_L-0")
     # fog
-    send_signal_to_sfarm("B0")
+    send_signal_to_ssfarm("B0")
     # window
-    send_signal_to_sfarm("S1")
+    send_signal_to_sfarm("C_S-1")
 
 # 여름 4단계
 def summer_forth():
     # fan
     send_signal_to_sfarm("C_F-1")
     # heat
-    send_signal_to_sfarm("RO")
+    send_signal_to_ssfarm("RO")
     # light
     send_signal_to_sfarm("C_L-5")
     # fog
-    send_signal_to_sfarm("B0")
+    send_signal_to_ssfarm("B0")
     # window
-    send_signal_to_sfarm("S1")
+    send_signal_to_sfarm("C_S-1")
 
 # 여름 5단계
 def summer_fifth():
     # fan
     send_signal_to_sfarm("C_F-1")
     # heat
-    send_signal_to_sfarm("RO")
+    send_signal_to_ssfarm("RO")
     # light
     send_signal_to_sfarm("C_L-8")
     # fog
-    send_signal_to_sfarm("B0")
+    send_signal_to_ssfarm("B0")
     # window
-    send_signal_to_sfarm("S1")
+    send_signal_to_sfarm("C_S-1")
 
 # 겨울 1단계
 def winter_first():
     # fan
     send_signal_to_sfarm("C_F-1")
     # heat
-    send_signal_to_sfarm("R1")
+    send_signal_to_ssfarm("R1")
     # light
     send_signal_to_sfarm("C_L-9")
     # fog
-    send_signal_to_sfarm("B1")
+    send_signal_to_ssfarm("B1")
     # window
-    send_signal_to_sfarm("S0")
+    send_signal_to_sfarm("C_S-0")
 
 # 겨울 2단계
 def winter_second():
     # fan
     send_signal_to_sfarm("C_F-0")
     # heat
-    send_signal_to_sfarm("R1")
+    send_signal_to_ssfarm("R1")
     # light
     send_signal_to_sfarm("C_L-9")
     # fog
-    send_signal_to_sfarm("B1")
+    send_signal_to_ssfarm("B1")
     # window
-    send_signal_to_sfarm("S0")
+    send_signal_to_sfarm("C_S-0")
 
 # 겨울 3-1단계
 def winter_third1():
     # fan
     send_signal_to_sfarm("C_F-0")
     # heat
-    send_signal_to_sfarm("R1")
+    send_signal_to_ssfarm("R1")
     # light
     send_signal_to_sfarm("C_L-7")
     # fog
-    send_signal_to_sfarm("B1")
+    send_signal_to_ssfarm("B1")
     # window
-    send_signal_to_sfarm("S1")
+    send_signal_to_sfarm("C_S-1")
 
 # 겨울 3-2단계
 def winter_third2():
     # fan
     send_signal_to_sfarm("C_F-0")
     # heat
-    send_signal_to_sfarm("R1")
+    send_signal_to_ssfarm("R1")
     # light
     send_signal_to_sfarm("C_L-0")
     # fog
-    send_signal_to_sfarm("B1")
+    send_signal_to_ssfarm("B1")
     # window
-    send_signal_to_sfarm("S0")
+    send_signal_to_sfarm("C_S-0")
 
 # 겨울 4단계
 def winter_forth():
     # fan
     send_signal_to_sfarm("C_F-0")
     # heat
-    send_signal_to_sfarm("R1")
+    send_signal_to_ssfarm("R1")
     # light
     send_signal_to_sfarm("C_L-6")
     # fog
-    send_signal_to_sfarm("B1")
+    send_signal_to_ssfarm("B1")
     # window
-    send_signal_to_sfarm("S0")
+    send_signal_to_sfarm("C_S-0")
 
 # 겨울 5단계
 def winter_fifth():
     # fan
     send_signal_to_sfarm("C_F-0")
     # heat
-    send_signal_to_sfarm("R1")
+    send_signal_to_ssfarm("R1")
     # light
     send_signal_to_sfarm("C_L-8")
     # fog
-    send_signal_to_sfarm("B1")
+    send_signal_to_ssfarm("B1")
     # window
-    send_signal_to_sfarm("S0")
+    send_signal_to_sfarm("C_S-0")
 
 # 비 1단계
 def rain_first():
     # fan
     send_signal_to_sfarm("C_F-1")
     # heat
-    send_signal_to_sfarm("R1")
+    send_signal_to_ssfarm("R1")
     # light
     send_signal_to_sfarm("C_L-9")
     # fog
-    send_signal_to_sfarm("B0")
+    send_signal_to_ssfarm("B0")
     # window
-    send_signal_to_sfarm("S0")
+    send_signal_to_sfarm("C_S-0")
 
 # 비 2단계
 def rain_second():
     # fan
     send_signal_to_sfarm("C_F-1")
     # heat
-    send_signal_to_sfarm("RO")
+    send_signal_to_ssfarm("RO")
     # light
     send_signal_to_sfarm("C_L-7")
     # fog
-    send_signal_to_sfarm("B0")
+    send_signal_to_ssfarm("B0")
     # window
-    send_signal_to_sfarm("S0")
+    send_signal_to_sfarm("C_S-0")
 
 # 비 3-1단계
 def rain_third1():
     # fan
     send_signal_to_sfarm("C_F-1")
     # heat
-    send_signal_to_sfarm("R1")
+    send_signal_to_ssfarm("R1")
     # light
     send_signal_to_sfarm("C_L-6")
     # fog
-    send_signal_to_sfarm("B0")
+    send_signal_to_ssfarm("B0")
     # window
-    send_signal_to_sfarm("S0")
+    send_signal_to_sfarm("C_S-0")
 
 # 비 3-2단계
 def rain_third2():
     # fan
     send_signal_to_sfarm("C_F-1")
     # heat
-    send_signal_to_sfarm("RO")
+    send_signal_to_ssfarm("RO")
     # light
     send_signal_to_sfarm("C_L-0")
     # fog
-    send_signal_to_sfarm("B0")
+    send_signal_to_ssfarm("B0")
     # window
-    send_signal_to_sfarm("S0")
+    send_signal_to_sfarm("C_S-0")
 
 # 비 4단계
 def rain_forth():
     # fan
     send_signal_to_sfarm("C_F-1")
     # heat
-    send_signal_to_sfarm("RO")
+    send_signal_to_ssfarm("RO")
     # light
     send_signal_to_sfarm("C_L-5")
     # fog
-    send_signal_to_sfarm("B0")
+    send_signal_to_ssfarm("B0")
     # window
-    send_signal_to_sfarm("S0")
+    send_signal_to_sfarm("C_S-0")
 
 # 비 5단계
 def rain_fifth():
     # fan
     send_signal_to_sfarm("C_F-1")
     # heat
-    send_signal_to_sfarm("R1")
+    send_signal_to_ssfarm("R1")
     # light
     send_signal_to_sfarm("C_L-7")
     # fog
-    send_signal_to_sfarm("B0")
+    send_signal_to_ssfarm("B0")
     # window
-    send_signal_to_sfarm("S0")
+    send_signal_to_sfarm("C_S-0")
 
 
 # 초 단위로 실행
@@ -283,11 +286,12 @@ while True:
     schedule.every(10).seconds.do(summer_third2)
     schedule.every(10).seconds.do(winter_third2)
     schedule.every(10).seconds.do(rain_third2)
-    
+
     # 개화 적온
     schedule.every(10).seconds.do(summer_forth)
     schedule.every(10).seconds.do(winter_forth)
     schedule.every(10).seconds.do(rain_forth)
+
 
     # 과비대 적온
     schedule.every(10).seconds.do(summer_fifth)
